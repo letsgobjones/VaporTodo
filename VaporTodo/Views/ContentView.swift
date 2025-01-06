fb5d387036298dbbc926db4dad1bd1ce33026f59
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-01-07 08:24:37 +0900

    Refactored to separate functions and general code clean up.

--- VaporTodo/Views/ContentView.swift
+++ VaporTodo/Views/ContentView.swift
@@ -6,11 +6,12 @@
 //
 
 import SwiftUI
-import SwiftData
+//import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var todo: [TodoItem]
+//    @Query private var todo: [TodoItem]
+  @ObservedObject var todoStore: TodoStore
 
   @State var title: String = ""
   @State var dueDate: Date = Date().addingTimeInterval(604800)
@@ -18,7 +19,7 @@ struct ContentView: View {
   @State var priority: Int = 0
     var body: some View {
             List {
-                ForEach(todo) { item in
+              ForEach(todoStore.todos) { item in
                     NavigationLink {
 //                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
                       
@@ -28,14 +29,14 @@ struct ContentView: View {
                       Text(item.title)
                     }
                 }
-                .onDelete(perform: deleteItems)
+                .onDelete(perform: todoStore.deleteTodo)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
                 ToolbarItem {
-                  NavigationLink(destination: AddTodoView()) {
+                  NavigationLink(destination: AddTodoView(todoStore: todoStore)) {
                     Label("Add Item", systemImage: "plus")
                   }
                   
@@ -51,27 +52,32 @@ struct ContentView: View {
         
     }
 
-    private func addItem() {
-        withAnimation {
-          let newItem = TodoItem(title: title, isCompleted: isCompleted, dueDate: dueDate, priority: priority,timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(todo[index])
-            }
-        }
-    }
 }
 
 #Preview {
   NavigationStack {
-    ContentView()
+    ContentView(todoStore: TodoStore())
     //      .modelContainer(for: TodoItem.self, inMemory: true)
   }
-  .modelContainer(PreviewContainer.shared)
+//  .modelContainer(PreviewContainer.shared)
   
 }
+
+
+//extension ContentView {
+//  private func addItem() {
+//      withAnimation {
+//        let newItem = TodoItem(title: title, isCompleted: isCompleted, dueDate: dueDate, priority: priority,timestamp: Date())
+//          modelContext.insert(newItem)
+//      }
+//  }
+//
+//  private func deleteItems(offsets: IndexSet) {
+//      withAnimation {
+//          for index in offsets {
+//              modelContext.delete(todo[index])
+//          }
+//      }
+//  }
+//}
