14885a8e91f6fa35de47d48a5ce784dfb581c02c
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-01-07 08:26:17 +0900

    Commented out SwiftData code.

--- VaporTodo/VaporTodoApp.swift
+++ VaporTodo/VaporTodoApp.swift
@@ -10,25 +10,25 @@ import SwiftData
 
 @main
 struct VaporTodoApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            TodoItem.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+//    var sharedModelContainer: ModelContainer = {
+//        let schema = Schema([
+//            TodoItem.self,
+//        ])
+//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+//
+//        do {
+//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+//        } catch {
+//            fatalError("Could not create ModelContainer: \(error)")
+//        }
+//    }()
 
     var body: some Scene {
       WindowGroup {
         NavigationStack {
-          ContentView()
+          ContentView(todoStore: TodoStore())
         }
-        .modelContainer(sharedModelContainer)
+//        .modelContainer(sharedModelContainer)
       }
     }
 }
