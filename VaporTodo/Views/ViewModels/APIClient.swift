795c0e4d0bccf58ed7c80fb4f953e3d804685a64
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-01-07 08:25:25 +0900

    Implemented a way to talk to the server and perform CRUD.

--- VaporTodo/Views/ViewModels/APIClient.swift
+++ VaporTodo/Views/ViewModels/APIClient.swift
@@ -16,6 +16,7 @@ struct APIClient {
         case invalidURL
         case requestFailed
         case decodingError
+      case encodingError
     }
 
   func fetchTodos() async throws -> [TodoItem] {
@@ -37,6 +38,41 @@ struct APIClient {
             throw APIError.decodingError
         }
     }
+  
+  
+  
+  func createTodo(title: String, isCompleted: Bool, dueDate: Date, priority: Int) async throws -> TodoItem {
+          guard let url = URL(string: "todos", relativeTo: baseURL) else {
+              throw APIError.invalidURL
+          }
+
+          // Create the request body (JSON encoding)
+          let newTodo = TodoItem(title: title, isCompleted: isCompleted, priority: priority)
+          guard let jsonData = try? JSONEncoder().encode(newTodo) else {
+              throw APIError.encodingError
+          }
+
+          var request = URLRequest(url: url)
+          request.httpMethod = "POST"
+          request.httpBody = jsonData
+          request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+          let (data, response) = try await URLSession.shared.data(for: request)
+
+          guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+              throw APIError.requestFailed
+          }
+
+          return try JSONDecoder().decode(TodoItem.self, from: data)
+      }
+  
+  
+  
+  
+  
+  
+  
+  
 
     // ... add other methods for creating, updating, deleting todos
 }
