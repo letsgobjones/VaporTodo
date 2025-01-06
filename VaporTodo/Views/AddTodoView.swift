185da7fab25cf0dbf3205e90256e1f9c25b26899
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-01-07 08:24:12 +0900

    Refactored AddTodoView to separate functions and removed time/ date.

--- VaporTodo/Views/AddTodoView.swift
+++ VaporTodo/Views/AddTodoView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct AddTodoView: View {
   @Environment(\.dismiss) private var dismiss
-  @Environment(\.modelContext) private var modelContext
+//  @Environment(\.modelContext) private var modelContext
+  @ObservedObject var todoStore: TodoStore
 
   @State var title: String = ""
   @State var dueDate: Date = Date().addingTimeInterval(604800)
@@ -24,32 +25,36 @@ struct AddTodoView: View {
         
         TextField("Priority", value: $priority, format: .number)
         
-        DatePicker("Due Date", selection: $dueDate)
+//        DatePicker("Due Date", selection: $dueDate)
+        DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
         Toggle("Completed", isOn: $isCompleted)
         
         
       }
       Button("Save") {
-        addItem()
+        Task {
+          await todoStore.createTodo(title: title, isCompleted: isCompleted, dueDate: dueDate, priority: priority)
+          dismiss()
+        }
       }
     }
 }
 
 #Preview {
   NavigationStack {
-    AddTodoView()
+    AddTodoView(todoStore: TodoStore())
   }
-      .modelContainer(PreviewContainer.shared)
+//      .modelContainer(PreviewContainer.shared)
 }
 
-extension AddTodoView {
-  private func addItem() {
-      withAnimation {
-          let newItem = TodoItem(title: title, isCompleted: isCompleted, dueDate: dueDate, priority: priority,timestamp: Date())
-          modelContext.insert(newItem)
-        dismiss()
-        print("Was saved")
-        print(newItem)
-      }
-  }
-}
+//extension AddTodoView {
+//  private func addItem() {
+//      withAnimation {
+//          let newItem = TodoItem(title: title, isCompleted: isCompleted, dueDate: dueDate, priority: priority,timestamp: Date())
+//          modelContext.insert(newItem)
+//        dismiss()
+//        print("Was saved")
+//        print(newItem)
+//      }
+//  }
+//}
