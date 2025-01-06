edaa768e8e401322a72cfc2f219ae8d709d3f53b
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-01-06 12:52:43 +0900

    Created a PreviewContainer to display and manage data.

---
+++ VaporTodo/Models/PreviewContainer.swift
@@ -0,0 +1,24 @@
+//
+//  PreviewContainer.swift
+//  VaporTodo
+//
+//  Created by Brandon Jones on 1/6/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct PreviewContainer {
+    static var shared: ModelContainer = {
+        let schema = Schema([
+            TodoItem.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+        do {
+            let container = try ModelContainer(for: schema, configurations: modelConfiguration)
+            return container
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+}
